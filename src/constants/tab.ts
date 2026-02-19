128eae72ee5f596187272374ac40845642d4a0c4
Rei WuZen <reiwuzen@gmail.com>
2026-02-19 23:33:49 +0530

    refactor(constants/tab): remove unused tab React components

--- src/constants/tab.ts
+++ src/constants/tab.ts
@@ -1,15 +1,13 @@
 import { TabType } from "@/types/tab";
 import Create from "@/workspace/create/create";
-import MemorySpace from "@/workspace/memory_space/memory_space";
+import Library from "@/workspace/library/library";
 import Overview from "@/workspace/overview/overview";
-import Settings from "@/workspace/settings/settings";
 import Structure from "@/workspace/structure/structure";
 
 export const TAB_COMPONENTS: Record<TabType, React.ComponentType> = {
   overview: Overview,
-  memory_space: MemorySpace,
+  library: Library,
   create: Create,
   structure: Structure,
-  settings: Settings,
   // editor: Editor,
 };
